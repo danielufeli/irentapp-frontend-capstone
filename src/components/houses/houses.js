f14f13406e755f6a1d731401dae8f7f4655e0cd0
david-lafontant <lafontant.david@gmail.com>
2022-07-03 06:46:42 -0600

    Fix linter errors

--- src/components/houses/houses.js
+++ src/components/houses/houses.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getHouses } from "../../features/houses/housesState";
-import Aside from "../sidebar/sidebar";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHouses } from '../../features/houses/housesState';
+import Aside from '../sidebar/sidebar';
 
 function Houses() {
   const dispatch = useDispatch();
@@ -12,19 +11,18 @@ function Houses() {
     dispatch(getHouses());
   }, [dispatch]);
 
-
   return (
     <div className="container d-flex flex-column flex-md-row">
       <Aside />
       <main className="ps-0 ps-md-5 flex-grow-1 pt-3">
 
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="carousel slide multi-item-carousel" id="theCarousel">
-                <div class="carousel-inner">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="carousel slide multi-item-carousel" id="theCarousel">
+                <div className="carousel-inner">
                   {houses.map((item) => (
-                    <div className="col-md-4 mb-3 item">
+                    <div className="col-md-4 mb-3 item" key={item.id}>
                       <div className="card" id={item.id}>
                         <img
                           className="img-fluid"
@@ -33,26 +31,30 @@ function Houses() {
                         />
                         <div className="card-body">
                           <h4 className="card-title">{item.name}</h4>
-                          <p className="card-text">{item.price} per month</p>
-                          <button>More details</button>
+                          <p className="card-text">
+                            {item.price}
+                            {' '}
+                            per month
+                          </p>
+                          <button type="button">More details</button>
                         </div>
                       </div>
                     </div>
                   ))}
                 </div>
                 <a
-                  class="left carousel-control"
+                  className="left carousel-control"
                   href="#theCarousel"
                   data-slide="prev"
                 >
-                  <i class="glyphicon glyphicon-chevron-left"></i>
+                  <i className="glyphicon glyphicon-chevron-left" />
                 </a>
                 <a
-                  class="right carousel-control"
+                  className="right carousel-control"
                   href="#theCarousel"
                   data-slide="next"
                 >
-                  <i class="glyphicon glyphicon-chevron-right"></i>
+                  <i className="glyphicon glyphicon-chevron-right" />
                 </a>
               </div>
             </div>
