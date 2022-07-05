94dc0b0ae1d50155129a1076f5fa7a7a849a7045
david-lafontant <lafontant.david@gmail.com>
2022-07-04 18:34:09 -0600

    Use axios to fetch and post - add Post

--- src/features/houses/housesState.js
+++ src/features/houses/housesState.js
@@ -1,42 +1,43 @@
 /* eslint-disable */
 
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const URL = 'http://127.0.0.1:3000/api/v1/houses'
+const HOUSES_URL = 'http://127.0.0.1:3000/api/v1/houses';
 
-export const getHouses = createAsyncThunk (
-'photos/getHouses',
-async () => {
-const response = await fetch(URL);
-const data = await response.json();
-return data;
-}
+export const getHouses = createAsyncThunk('houses/getHouses', async () => {
+    const response = await axios.get(HOUSES_URL)
+    return response.data
+})
 
-);
+export const addNewHouse = createAsyncThunk('houses/addNewHouse', async (obj) => {
+	const response = await axios.post(HOUSES_URL, obj);
+	return response.data;
+})
 
 export const housesSlice= createSlice({
-name: 'gallery',
-initialState: {
-	items: [],
-	isLoading: false
-
-},
-extraReducers:{
-[getHouses.pending]: (state) => {
-	state.isLoading = true;
-},
-
-[getHouses.fulfilled]: (state, action) =>{
-	state.items = action.payload;
-	state.isLoading = false;
-},
-
-[getHouses.rejected]: (state) => {
-	state.isLoading = false
-}
-
-}
-
-});
-
-export default housesSlice.reducer
+  name: 'gallery',
+  initialState: {
+    items: [],
+    isLoading: false
+  
+  },
+  extraReducers:{
+  [getHouses.pending]: (state) => {
+    state.isLoading = true;
+  },
+  
+  [getHouses.fulfilled]: (state, action) =>{
+    state.items = action.payload;
+    state.isLoading = false;
+  },
+  
+  [getHouses.rejected]: (state) => {
+    state.isLoading = false
+  }
+  
+  }
+  
+  });
+  
+  export default housesSlice.reducer
